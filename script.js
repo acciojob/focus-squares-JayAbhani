c47ebd77e8b3c01e68a4bb8f8923003526dfadf7
Jay Abhani <abhani57@gmail.com>
2025-09-22 11:07:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+// Select all squares
+const squares = document.querySelectorAll(".square");
+
+// Colors
+const lavender = "#E6E6FA";
+const coffee = "#6F4E37";
+
+// Loop through each square
+squares.forEach((sq) => {
+  // When mouse enters one square
+  sq.addEventListener("mouseenter", () => {
+    squares.forEach((otherSq) => {
+      if (otherSq !== sq) {
+        otherSq.style.backgroundColor = coffee;
+      }
+    });
+  });
+
+  // When mouse leaves that square
+  sq.addEventListener("mouseleave", () => {
+    squares.forEach((s) => {
+      s.style.backgroundColor = lavender;
+    });
+  });
+});
